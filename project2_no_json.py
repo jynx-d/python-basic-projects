3e2e46695ba0ae6e23a4e9eca1a7465325974379
jynx-d <poeticdibbs03@gmail.com>
2026-01-01 20:17:24 +0530

    Add files via upload

---
+++ project2_no_json.py
@@ -0,0 +1,88 @@
+# list the questions
+# store the answers 
+# randomly pick questions
+# ask the questions 
+# see if they are correct
+# keep track of the score
+# tell the user their score
+# save & sort the highscores
+# show the highscores at the start 
+
+import random # to generate random questions
+questions = {
+    "What is the keyword to define a function in Python?": "def",
+    "Which data type is used to store True or False values?": "bool",
+    "What is the correct file extension for Python files?": ".py",
+    "Which symbol is used to comment in Python?": "#",
+    "What function is used to get input from the user?": "input",
+    "How do you start a for loop in Python?": "for",
+    "What is the output of 2 ** 3 in Python?": "8",
+    "What keyword is used to import a module in Python?": "import",
+    "What does the len() function return?": "length",
+    "What is the result of 10 // 3 in Python?": "3"
+}
+
+def display_high_scores():
+    print("\n TOP 5 HIGH SCORES:")
+    try:
+        with open("highscores.txt", "r") as f:
+            for i, line in enumerate(f, 1):
+                name, score = line.strip().split(",")
+                print(f"{i}. {name}: {score}/5")
+                
+    except FileNotFoundError:
+        print("No high scores yet!")
+
+def save_high_score(name, score):
+      
+    scores = []
+    try:
+        with open("highscores.txt", "r") as f:  # Read all existing scores
+            for line in f:
+                og_name, og_score = line.strip().split(",")
+                scores.append((og_name, int(og_score)))
+    except FileNotFoundError:
+        pass
+    
+    scores.append((name, score)) # Add new score
+    
+    scores.sort(key=lambda x: x[1], reverse=True) # Sort by score (descending)
+    
+    scores = scores[:5] # Keep only top 5 and overwrite file
+    
+    with open('highscores.txt', 'w') as f: # Write sorted top 5 back to file
+        for n, s in scores:
+            f.write(f"{n},{s}\n")
+    
+    print("High scores updated!")
+   
+
+def python_trivia_game():
+    name = input("Enter your name: ")
+
+    display_high_scores()
+    input("\nPress Enter to start...\n")
+
+    questions_list = list(questions.keys())
+    total_questions = 5
+    score = 0
+
+    selected_questions = random.sample(questions_list, total_questions)
+
+    for idx,question in enumerate(selected_questions):
+        print(f"{idx + 1}. {question}")
+        user_answer = input("Your answer: ").lower().strip()
+        correct_answer = questions[question]
+
+        if user_answer == correct_answer.lower():
+            print("Correct answer!\n")
+            score += 1
+        else:
+            print(f"Wrong. The correct answer is: {correct_answer}. \n") 
+
+    print(f"Game over! Your final score is: {score}/{total_questions}")  
+    save_high_score(name,score)
+
+ 
+
+python_trivia_game()    
